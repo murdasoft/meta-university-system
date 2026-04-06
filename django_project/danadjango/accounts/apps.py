b05da3cc856d5640bf1f3f18f153597101b0c8f6
Allexndr <sanmemik2000@gmail.com>
2026-04-06 14:51:14 +0500

    Fix Vercel 500: Disconnect update_last_login signal for read-only SQLite

--- django_project/danadjango/accounts/apps.py
+++ django_project/danadjango/accounts/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
-
+from django.contrib.auth.signals import user_logged_in
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        # Disconnect update_last_login signal because Vercel's SQLite is read-only
+        try:
+            from django.contrib.auth.models import update_last_login
+            user_logged_in.disconnect(update_last_login)
+        except ImportError:
+            pass
